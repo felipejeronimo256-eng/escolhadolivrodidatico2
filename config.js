080c9b5a5aabe1ee6d584f66656c112890d66851
felipejeronimo256-eng <felipe.jeronimo256@gmail.com>
2026-08-27 09:51:55 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,6 @@
 // Console Firebase > Configurações do projeto > Seus apps > SDK setup and configuration.
 // Este mesmo arquivo pode ser reaproveitado pelo resultados.html, se ele
 // também usar o Firestore.
-
 const firebaseConfig = {
   apiKey: "AIzaSyAyD9d9Su4CxAkUaq8jyPyIwNyGGK0nCkE",
   authDomain: "escolhadolivrodidaticopmv.firebaseapp.com",
@@ -12,4 +11,3 @@ const firebaseConfig = {
   messagingSenderId: "538840478739",
   appId: "1:538840478739:web:17225357871c7e9a1c3be6"
 };
-const app = initializeApp(firebaseConfig);
